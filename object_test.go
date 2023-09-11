bed2943b4ec6b4f399d281c755212edccdce1f1d
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2023-09-11 18:08:48 -0400

    Update and expand tests

--- object_test.go
+++ object_test.go
@@ -21,7 +21,7 @@ func ExampleNewObject() {
 		Age:  -1,
 	}
 
-	tp, ta := xcel.NewTypeProvider(), xcel.NewTypeAdapter()
+	ta, tp := xcel.NewTypeAdapter(), xcel.NewTypeProvider()
 
 	obj, typ := xcel.NewObject(ta, tp, person, "Person")
 
@@ -89,15 +89,30 @@ func ExampleNewObject() {
 	// Output: false
 }
 
-func TestWrapper(t *testing.T) {
-	type Example struct {
-		Name string
+type Example struct {
+	Name     string
+	Age      int
+	Tags     []string
+	Parent   *Example
+	Pressure float64
+}
+
+func TestNewObject(t *testing.T) {
+	ta, tp := xcel.NewTypeAdapter(), xcel.NewTypeProvider()
+
+	ex := &Example{
+		Name: "test",
+		Age:  1,
+		Tags: []string{"test"},
+		Parent: &Example{
+			Name: "root",
+			Age:  -1,
+			Tags: []string{"a", "b", "c"},
+		},
+		Pressure: 1.5,
 	}
 
-	tp := xcel.NewTypeProvider()
-	ta := xcel.NewTypeAdapter()
-
-	obj, typ := xcel.NewObject(ta, tp, &Example{Name: "test"}, "Example")
+	obj, typ := xcel.NewObject(ta, tp, ex, "Example")
 
 	xcel.RegisterObject(ta, tp, obj, typ, map[string]*types.FieldType{
 		"name": {
@@ -121,6 +136,92 @@ func TestWrapper(t *testing.T) {
 				return objW.Raw.Name, nil
 			}),
 		},
+		"age": {
+			Type: types.IntType,
+			IsSet: ref.FieldTester(func(target any) bool {
+				x := target.(*xcel.Object[*Example])
+
+				if x.Raw == nil || x.Raw.Age < 0 {
+					return false
+				}
+
+				return true
+			}),
+			GetFrom: ref.FieldGetter(func(target any) (any, error) {
+				x := target.(*xcel.Object[*Example])
+
+				if x.Raw == nil {
+					return nil, fmt.Errorf("celval: object is nil")
+				}
+
+				return x.Raw.Age, nil
+			}),
+		},
+		"tags": {
+			Type: types.NewListType(types.StringType),
+			IsSet: ref.FieldTester(func(target any) bool {
+				x := target.(*xcel.Object[*Example])
+
+				if x.Raw == nil || len(x.Raw.Tags) == 0 {
+					return false
+				}
+
+				return true
+			}),
+			GetFrom: ref.FieldGetter(func(target any) (any, error) {
+				x := target.(*xcel.Object[*Example])
+
+				if x.Raw == nil {
+					return nil, fmt.Errorf("celval: object is nil")
+				}
+
+				return types.NewStringList(ta, x.Raw.Tags), nil
+			}),
+		},
+		"parent": {
+			Type: typ,
+			IsSet: ref.FieldTester(func(target any) bool {
+				x := target.(*xcel.Object[*Example])
+
+				if x.Raw == nil || x.Raw.Parent == nil {
+					return false
+				}
+
+				return true
+			}),
+			GetFrom: ref.FieldGetter(func(target any) (any, error) {
+				x := target.(*xcel.Object[*Example])
+
+				if x.Raw == nil {
+					return nil, fmt.Errorf("celval: object is nil")
+				}
+
+				obj, _ := xcel.NewObject(ta, tp, x.Raw.Parent, "Example")
+
+				return obj, nil
+			}),
+		},
+		"pressure": {
+			Type: types.DoubleType,
+			IsSet: ref.FieldTester(func(target any) bool {
+				x := target.(*xcel.Object[*Example])
+
+				if x.Raw == nil || x.Raw.Pressure <= 0 {
+					return false
+				}
+
+				return true
+			}),
+			GetFrom: ref.FieldGetter(func(target any) (any, error) {
+				x := target.(*xcel.Object[*Example])
+
+				if x.Raw == nil {
+					return nil, fmt.Errorf("celval: object is nil")
+				}
+
+				return x.Raw.Pressure, nil
+			}),
+		},
 	})
 
 	env, err := cel.NewEnv(
@@ -134,7 +235,7 @@ func TestWrapper(t *testing.T) {
 		t.Fatalf("failed to create CEL environment: %v", err)
 	}
 
-	ast, iss := env.Compile("obj.name")
+	ast, iss := env.Compile("obj.name == 'test' && obj.age > 0 && ('test' in obj.tags) && obj.parent.name == 'root' && obj.pressure > 1.0")
 	if iss.Err() != nil {
 		t.Fatalf("failed to compile CEL expression: %v", iss.Err())
 	}
@@ -151,7 +252,7 @@ func TestWrapper(t *testing.T) {
 		t.Fatalf("failed to evaluate program: %v", err)
 	}
 
-	if fmt.Sprintf("%v", out.Value()) != "test" {
-		t.Fatalf("expected 'test' but got '%v'", out.Value())
+	if fmt.Sprintf("%v", out.Value()) != "true" {
+		t.Fatalf("expected 'true' but got '%v'", out.Value())
 	}
 }
